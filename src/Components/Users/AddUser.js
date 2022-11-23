d4ae593e58a3955ce6cbde3aec8edcd1969ea4d0
Blank67 <hmnshshrm67@gmail.com>
2022-11-23 15:41:45 +0530

    using fragments

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import ErrorModal from "../UI/ErrorModal";
@@ -18,21 +18,21 @@ const AddUser = (props) => {
     };
     const addUserHandler = (event) => {
         event.preventDefault();
-        if(enteredUserAge.trim().length === 0 || enteredUserName.trim().length === 0){
+        if (enteredUserAge.trim().length === 0 || enteredUserName.trim().length === 0) {
             setError({
                 title: "Invalid Input",
                 message: "Name or Age cannot be empty."
-            }); 
-            return;   
+            });
+            return;
         }
-        if(enteredUserAge < 1){
+        if (enteredUserAge < 1) {
             setError({
                 title: "Invalid Age",
                 message: "Age should be greater than 0."
-            });    
-            return;   
+            });
+            return;
         }
-        props.onAdd(enteredUserName,enteredUserAge);
+        props.onAdd(enteredUserName, enteredUserAge);
         setEnteredUserName('');
         setEnteredUserAge('');
     };
@@ -41,16 +41,19 @@ const AddUser = (props) => {
     };
 
     return (
-        <Card className={css.input}>
-            {error && <ErrorModal title={error.title} message={error.message} onClick={errorHandler}/>}
-            <form onSubmit={addUserHandler}>
-                <label htmlFor="username">Name:</label>
-                <input id="username" type="text" value={enteredUserName} onChange={enteredUserNameChnageHandler} />
-                <label htmlFor="userage">Age (in Years):</label>
-                <input id="userage" type="number" value={enteredUserAge} onChange={enteredUserAgeChnageHandler} />
-                <Button type="submit" name={enteredUserName} age={enteredUserAge}>Add</Button>
-            </form>
-        </Card>
+        //We can user Wrapper Component(User created component that return props.children), <> </>, React.Fragment instead of <div> </div> as they don't actually load onn our DOM.
+        <Fragment> 
+            {error && <ErrorModal title={error.title} message={error.message} onClick={errorHandler} />}
+            <Card className={css.input}>
+                <form onSubmit={addUserHandler}>
+                    <label htmlFor="username">Name:</label>
+                    <input id="username" type="text" value={enteredUserName} onChange={enteredUserNameChnageHandler} />
+                    <label htmlFor="userage">Age (in Years):</label>
+                    <input id="userage" type="number" value={enteredUserAge} onChange={enteredUserAgeChnageHandler} />
+                    <Button type="submit" name={enteredUserName} age={enteredUserAge}>Add</Button>
+                </form>
+            </Card>
+        </Fragment>
     );
 };
 
